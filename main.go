891b157b21541ba0929f1ffb3a0022fa931246f1
Dan Rousseau <danrousseau@protonmail.com>
2023-02-08 15:42:23 +0200

    feat: add test function

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/thelazylemur/cacheengine/cache"
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/thelazylemur/cacheengine/cache"
+)
 
 func main(){
 	opts := ServerOpts {
@@ -8,6 +14,21 @@ func main(){
 		IsLeader: true,
 	}
 
+	go test()
+
 	server := NewServer(opts, cache.New())
-	server.Start()
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func test(){
+	time.Sleep(time.Second * 2)
+
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn.Write([]byte("SET Foo Bar 250000"))
 }
