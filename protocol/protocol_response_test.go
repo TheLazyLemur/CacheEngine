65a60694474e0befeba27c2a0b07bf554e117a0a
Dan Rousseau <danrousseau@protonmail.com>
2023-02-24 08:15:46 +0200

    chore: added new tests for response parsing

--- protocol/protocol_response_test.go
+++ protocol/protocol_response_test.go
@@ -1,47 +1,114 @@
 package protocol
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseSetResponseStatusNone(t *testing.T) {
+func TestParseSetResponse(t *testing.T) {
 	resp := &ResponseSet{
 		Status: StatusNone,
 	}
+	reader := bytes.NewReader(resp.Bytes())
+	actual, err := ParseSetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, actual)
 
-	expeced := []byte{0}
-	actual := resp.Bytes()
-	assert.Equal(t, expeced, actual)
-}
-
-func TestParseSetResponseStatusOk(t *testing.T) {
-	resp := &ResponseSet{
+	resp = &ResponseSet{
 		Status: StatusOK,
 	}
+	reader = bytes.NewReader(resp.Bytes())
+	actual, err = ParseSetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, actual)
 
-	expeced := []byte{1}
-	actual := resp.Bytes()
-	assert.Equal(t, expeced, actual)
-}
-
-func TestParseSetResponseStatusErr(t *testing.T) {
-	resp := &ResponseSet{
+	resp = &ResponseSet{
 		Status: StatusError,
 	}
+	reader = bytes.NewReader(resp.Bytes())
+	actual, err = ParseSetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, actual)
 
-	expeced := []byte{2}
-	actual := resp.Bytes()
-	assert.Equal(t, expeced, actual)
-}
-
-func TestParseSetResponseStatusKeyNotFound(t *testing.T) {
-	resp := &ResponseSet{
+	resp = &ResponseSet{
 		Status: StatusKeyNotFound,
 	}
+	reader = bytes.NewReader(resp.Bytes())
+	actual, err = ParseSetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, actual)
+}
 
-	expeced := []byte{3}
-	actual := resp.Bytes()
-	assert.Equal(t, expeced, actual)
+func TestParseGetResponseStatusOk(t *testing.T) {
+	resp := &ResponseGet{
+		Status: StatusOK,
+		Value:  []byte("value"),
+	}
+	reader := bytes.NewReader(resp.Bytes())
+	x, err := ParseGetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseGet{
+		Status: StatusError,
+		Value:  []byte(""),
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseGetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseGet{
+		Status: StatusKeyNotFound,
+		Value:  []byte(""),
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseGetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseGet{
+		Status: StatusNone,
+		Value:  []byte(""),
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseGetReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+}
+
+func TestParseDelResponseStatusOk(t *testing.T) {
+	resp := &ResponseDelete{
+		Status: StatusOK,
+	}
+	reader := bytes.NewReader(resp.Bytes())
+	x, err := ParseDelReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseDelete{
+		Status: StatusError,
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseDelReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseDelete{
+		Status: StatusKeyNotFound,
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseDelReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
+
+	resp = &ResponseDelete{
+		Status: StatusNone,
+	}
+	reader = bytes.NewReader(resp.Bytes())
+	x, err = ParseDelReponse(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, x)
 }
